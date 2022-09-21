4e899579c7250f990ad11b42406c45d6015137b6
jacob-bunker <jakebnkr@gmail.com>
2022-09-20 18:44:00 -0700

    tictactoe

--- developer/Unit01/Program.cs
+++ developer/Unit01/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Unit01
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /// tic tac toe - Jacob Bunker 
+            int board = BoardArea();
+
+       
+        }
+         static int BoardArea ()
+        {
+            Console.WriteLine("how big of a board do you want? ");
+            int BoardSize = int.Parse(Console.ReadLine());
+
+            return BoardSize;
+        }
+
+        static List<string> GetNewBoard(int BoardSize)
+        {
+            List<string> PlayArea = new List<string>();
+            for(int i = 1; i <= BoardSize; i++)
+            {
+                PlayArea.Add(i.ToString());
+            }
+            return PlayArea;
         }
     }
 }
