9302db0aaac5610f7ed310e2e3d913f9ff32607f
jacob-bunker <jakebnkr@gmail.com>
2022-09-21 15:09:33 -0700

    stuff

--- developer/Unit01/Program.cs
+++ developer/Unit01/Program.cs
@@ -7,26 +7,27 @@ namespace Unit01
         static void Main(string[] args)
         {
             /// tic tac toe - Jacob Bunker 
-            int board = BoardArea();
+            List<string> board = BoardArea();
 
        
         }
-         static int BoardArea ()
+         static List<string> BoardArea ()
         {
             Console.WriteLine("how big of a board do you want? ");
             int BoardSize = int.Parse(Console.ReadLine());
 
-            return BoardSize;
-        }
-
-        static List<string> GetNewBoard(int BoardSize)
-        {
             List<string> PlayArea = new List<string>();
             for(int i = 1; i <= BoardSize; i++)
             {
                 PlayArea.Add(i.ToString());
             }
             return PlayArea;
+            
         }
+
+        /*static List<string> GetNewBoard(int BoardSize)
+        {
+            
+        }*/
     }
 }
--- sandbox/SandboxProject/Program.cs
+++ sandbox/SandboxProject/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
-
-namespace SandboxProject
+using System.Collections.Generic;
+namespace Unit01
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /// tic tac toe - Jacob Bunker 
+            Console.WriteLine("how big of a board do you want? ");
+            int BoardSize = int.Parse(Console.ReadLine());
+            
+            List<string> boardNum = BoardArea(BoardSize);
+            
+       
+        }
+         static List<string> BoardArea (int BoardSize)
+        {
+            List<string> PlayArea = new List<string>();
+            for(int i = 1; i <= BoardSize * BoardSize; i++)
+            {
+                PlayArea.Add(i.ToString());
+            }
+            return PlayArea;
+            
+        }
+
+        static void GetNewBoard(int BoardSize,List<string> boardNum)
+        {
+            int SpaceNum = 0;
+            for(int i = 1; i <= BoardSize; i++)
+            {
+                for(int c = 1; c <= BoardSize; c++)
+                {
+                    Console.Write($"|{boardNum[SpaceNum]}");
+                    SpaceNum++;
+                }
+                Console.WriteLine('--');
+            }
         }
     }
 }
+
